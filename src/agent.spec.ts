1bdf610c0de2d14f184133091d99877d73b66237
jmt-genius <tarunjm23@gmail.com>
2024-07-23 00:11:03 +0530

    tests: added new test cases for checking all possibilities

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,234 +1,208 @@
-import { FindingType, FindingSeverity, Finding, HandleTransaction, TransactionEvent } from "forta-agent";
+import { HandleTransaction } from "forta-agent";
 import { provideHandleTransaction } from "./agent";
-import { createAddress } from "forta-agent-tools";
 import { TestTransactionEvent } from "forta-agent-tools/lib/test";
-import { Interface } from "ethers/lib/utils";
-import { BigNumber } from "ethers";
+import { createAddress } from "forta-agent-tools";
+import { createFinding } from "./findings";
 
-const CREATE_FUNCTION_ABI: string = "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds) public";
-const UPDATE_FUNCTION_ABI: string = "function updateAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds) public";
-const DELETE_FUNCTION_ABI: string = "function deleteAgent(uint256 agentId) public";
-const REGISTRY_ADDRESS: string = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
-const BOT_DEPLOYER_ADDRESS: string = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
+const mockDeployerAddress = createAddress("0x1");
+const mockRegistryAddress = createAddress("0x2");
+const mockAnotherAddress = createAddress("0x3");
 
-const TEST_ADDRESS = createAddress("0x123abc");
+const mockCreateAgentAbi =
+  "function createAgent(uint256 agentId, address, string metadata, uint256[] chainIds) external";
+const mockUpdateAgentAbi = "function updateAgent(uint256 agentId, string metadata, uint256[] chainIds) public";
+const mockDeleteAgentAbi = "function deleteAgent(uint256 agentId) public";
+const mockAnotherFunctionAbi = "function mockAnotherFunction(uint256 agentId, string metadata, uint256[] chainIds)";
 
-const MOCK_METADATA = {
-  agentId: "123",
-  metadata: "abcd",
-  chainIds: ["137"],
+const mockAgentId = 1337;
+const mockChainIds = [137];
+const mockMetadata = "MockMetadata";
+const mockArgs = {
+  agentId: mockAgentId,
+  metadata: mockMetadata,
+  chainIds: mockChainIds,
 };
 
-const MOCK_METADATA2 = {
-  agentId: "12345678",
-  metadata: "abcdefg",
-  chainIds: ["137"],
-};
+const mockCreateFinding = createFinding("Creation", mockDeployerAddress, mockArgs);
+const mockUpdateFinding = createFinding("Update", mockDeployerAddress, mockArgs);
+const mockDeleteFinding = createFinding("Deletion", mockDeployerAddress, { agentId: mockAgentId });
 
-const MOCK_FINDING = (alertId: string, name: string, agentId: string, metadata: string = "", chainIds: string = ""): Finding => {
-  return Finding.fromObject({
-    name: name,
-    description: `${name} by Nethermind`,
-    alertId: alertId,
-    severity: FindingSeverity.Info,
-    type: FindingType.Info,
-    protocol: "Forta",
-    metadata: {
-      agentId: agentId,
-      ...(metadata && { metadata }),
-      ...(chainIds && { chainIds }),
-    },
-  });
-};
-
-describe("Bot deployment, update, and deletion tracker", () => {
+describe("Nethermind Bots Deployment Detector", () => {
   let handleTransaction: HandleTransaction;
-  let proxy = new Interface([CREATE_FUNCTION_ABI, UPDATE_FUNCTION_ABI, DELETE_FUNCTION_ABI]);
-  let findings: Finding[];
+  let mockTxEvent: TestTransactionEvent;
 
   beforeAll(() => {
-    handleTransaction = provideHandleTransaction(CREATE_FUNCTION_ABI, REGISTRY_ADDRESS, BOT_DEPLOYER_ADDRESS);
+    handleTransaction = provideHandleTransaction(
+      mockDeployerAddress,
+      mockRegistryAddress,
+      mockCreateAgentAbi,
+      mockUpdateAgentAbi,
+      mockDeleteAgentAbi
+    );
   });
 
-  it("should return no Findings if no bots deployed, updated, or deleted", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent();
-    findings = await handleTransaction(mockTxEvent);
+  beforeEach(() => {
+    mockTxEvent = new TestTransactionEvent().setTo(mockRegistryAddress).setFrom(mockDeployerAddress);
+  });
+
+  it("returns empty findings if bot was not deployed by Nethermind", async () => {
+    const mockTxEvent = new TestTransactionEvent()
+      .setFrom(mockAnotherAddress)
+      .setTo(mockRegistryAddress)
+      .addTraces({
+        to: mockAnotherAddress,
+        from: mockDeployerAddress,
+        function: mockCreateAgentAbi,
+        arguments: [mockAgentId, mockAnotherAddress, mockMetadata, mockChainIds],
+      });
+
+    const findings = await handleTransaction(mockTxEvent);
     expect(findings).toStrictEqual([]);
   });
 
-  it("should return no Findings if tx is not sent from the deployer", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(TEST_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
+  it("returns empty findings if bot was deployed to wrong registry", async () => {
+    const mockTxEvent = new TestTransactionEvent()
+      .setFrom(mockDeployerAddress)
+      .setTo(mockAnotherAddress)
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("createAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA.metadata,
-          [BigNumber.from(MOCK_METADATA.chainIds[0])],
-        ],
+        to: mockAnotherAddress,
+        from: mockDeployerAddress,
+        function: mockCreateAgentAbi,
+        arguments: [mockAgentId, mockAnotherAddress, mockMetadata, mockChainIds],
       });
 
-    findings = await handleTransaction(mockTxEvent);
-
+    const findings = await handleTransaction(mockTxEvent);
     expect(findings).toStrictEqual([]);
   });
 
-  it("should return Findings if tx is sent from the deployer to create agent", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(BOT_DEPLOYER_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
+  it("returns empty findings if bot was updated to wrong registry", async () => {
+    const mockTxEvent = new TestTransactionEvent()
+      .setFrom(mockDeployerAddress)
+      .setTo(mockAnotherAddress)
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("createAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA.metadata,
-          [BigNumber.from(MOCK_METADATA.chainIds[0])],
-        ],
+        to: mockAnotherAddress,
+        from: mockDeployerAddress,
+        function: mockUpdateAgentAbi,
+        arguments: [mockAgentId, mockMetadata, mockChainIds],
       });
 
-    findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      MOCK_FINDING("NETH-1", "Bot Deployment", MOCK_METADATA.agentId, MOCK_METADATA.metadata, MOCK_METADATA.chainIds[0]),
-    ]);
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([]);
   });
 
-  it("should return Findings if tx is sent from the deployer to update agent", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(BOT_DEPLOYER_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
+  it("returns empty findings if tx is not a bot deployment, update, or deletion", async () => {
+    mockTxEvent.addTraces({
+      to: mockRegistryAddress,
+      from: mockDeployerAddress,
+      function: mockAnotherFunctionAbi,
+      arguments: [mockAgentId, mockMetadata, mockChainIds],
+    });
+
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("returns empty findings if no bot was deployed, updated, or deleted", async () => {
+    mockTxEvent = new TestTransactionEvent().setTo(mockRegistryAddress).setFrom(mockDeployerAddress);
+
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("returns a finding if a new bot was deployed", async () => {
+    mockTxEvent = new TestTransactionEvent()
+      .setTo(mockRegistryAddress)
+      .setFrom(mockDeployerAddress)
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("updateAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA.metadata,
-          [BigNumber.from(MOCK_METADATA.chainIds[0])],
-        ],
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockCreateAgentAbi,
+        arguments: [mockAgentId, mockDeployerAddress, mockMetadata, mockChainIds],
       });
 
-    findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      MOCK_FINDING("NETH-2", "Bot Update", MOCK_METADATA.agentId, MOCK_METADATA.metadata, MOCK_METADATA.chainIds[0]),
-    ]);
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([mockCreateFinding]);
   });
 
-  it("should return Findings if tx is sent from the deployer to delete agent", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(BOT_DEPLOYER_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
+  it("returns a finding if a bot was updated", async () => {
+    mockTxEvent = new TestTransactionEvent()
+      .setTo(mockRegistryAddress)
+      .setFrom(mockDeployerAddress)
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("deleteAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-        ],
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockUpdateAgentAbi,
+        arguments: [mockAgentId, mockMetadata, mockChainIds],
       });
 
-    findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      MOCK_FINDING("NETH-3", "Bot Deletion", MOCK_METADATA.agentId),
-    ]);
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([mockUpdateFinding]);
   });
 
-  it("should return Findings for multiple createAgent calls in one tx", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(BOT_DEPLOYER_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
+  it("returns a finding if a bot was deleted", async () => {
+    mockTxEvent = new TestTransactionEvent()
+      .setTo(mockRegistryAddress)
+      .setFrom(mockDeployerAddress)
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("createAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA.metadata,
-          [BigNumber.from(MOCK_METADATA.chainIds[0])],
-        ],
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockDeleteAgentAbi,
+        arguments: [mockAgentId],
+      });
+
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([mockDeleteFinding]);
+  });
+
+  it("returns multiple findings if there are multiple create/update/delete events", async () => {
+    mockTxEvent = new TestTransactionEvent()
+      .setTo(mockRegistryAddress)
+      .setFrom(mockDeployerAddress)
+      // CreateAgent function
+      .addTraces({
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockCreateAgentAbi,
+        arguments: [mockAgentId, mockDeployerAddress, mockMetadata, mockChainIds],
       })
+      // UpdateAgent function
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("createAgent"),
-        arguments: [
-          MOCK_METADATA2.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA2.metadata,
-          [BigNumber.from(MOCK_METADATA2.chainIds[0])],
-        ],
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockUpdateAgentAbi,
+        arguments: [mockAgentId, mockMetadata, mockChainIds],
+      })
+      // DeleteAgent function
+      .addTraces({
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockDeleteAgentAbi,
+        arguments: [mockAgentId],
+      })
+      // Another function
+      .addTraces({
+        to: mockRegistryAddress,
+        from: mockDeployerAddress,
+        function: mockAnotherFunctionAbi,
+        arguments: [mockAgentId, mockMetadata, mockChainIds],
       });
 
-    findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      MOCK_FINDING("NETH-1", "Bot Deployment", MOCK_METADATA.agentId, MOCK_METADATA.metadata, MOCK_METADATA.chainIds[0]),
-      MOCK_FINDING("NETH-1", "Bot Deployment", MOCK_METADATA2.agentId, MOCK_METADATA2.metadata, MOCK_METADATA2.chainIds[0]),
-    ]);
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([mockCreateFinding, mockUpdateFinding, mockDeleteFinding]);
   });
 
-  it("should return Findings for multiple updateAgent calls in one tx", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(BOT_DEPLOYER_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
+  it("returns empty findings if there's a bot update from the deployer but not to the registry", async () => {
+    const mockTxEvent = new TestTransactionEvent()
+      .setFrom(mockDeployerAddress)
+      .setTo(mockAnotherAddress)
       .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("updateAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA.metadata,
-          [BigNumber.from(MOCK_METADATA.chainIds[0])],
-        ],
-      })
-      .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("updateAgent"),
-        arguments: [
-          MOCK_METADATA2.agentId,
-          TEST_ADDRESS,
-          MOCK_METADATA2.metadata,
-          [BigNumber.from(MOCK_METADATA2.chainIds[0])],
-        ],
+        to: mockAnotherAddress,
+        from: mockDeployerAddress,
+        function: mockUpdateAgentAbi,
+        arguments: [mockAgentId, mockMetadata, mockChainIds],
       });
 
-    findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      MOCK_FINDING("NETH-2", "Bot Update", MOCK_METADATA.agentId, MOCK_METADATA.metadata, MOCK_METADATA.chainIds[0]),
-      MOCK_FINDING("NETH-2", "Bot Update", MOCK_METADATA2.agentId, MOCK_METADATA2.metadata, MOCK_METADATA2.chainIds[0]),
-    ]);
-  });
-
-  it("should return Findings for multiple deleteAgent calls in one tx", async () => {
-    const mockTxEvent: TransactionEvent = new TestTransactionEvent()
-      .setFrom(BOT_DEPLOYER_ADDRESS)
-      .setTo(REGISTRY_ADDRESS)
-      .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("deleteAgent"),
-        arguments: [
-          MOCK_METADATA.agentId,
-        ],
-      })
-      .addTraces({
-        to: REGISTRY_ADDRESS,
-        function: proxy.getFunction("deleteAgent"),
-        arguments: [
-          MOCK_METADATA2.agentId,
-        ],
-      });
-
-    findings = await handleTransaction(mockTxEvent);
-
-    expect(findings).toStrictEqual([
-      MOCK_FINDING("NETH-3", "Bot Deletion", MOCK_METADATA.agentId),
-      MOCK_FINDING("NETH-3", "Bot Deletion", MOCK_METADATA2.agentId),
-    ]);
+    const findings = await handleTransaction(mockTxEvent);
+    expect(findings).toStrictEqual([]);
   });
 });
