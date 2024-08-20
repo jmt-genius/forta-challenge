5c502ae806e0c4c1321f32aa876ce8fdc44c1c6b
jmt-genius <tarunjm23@gmail.com>
2024-08-21 00:32:58 +0530

    feat: adding agent.ts

--- src/agent.ts
+++ src/agent.ts
@@ -1,92 +1,103 @@
-import {
-  BlockEvent,
-  Finding,
-  Initialize,
-  HandleBlock,
-  HealthCheck,
-  HandleTransaction,
-  HandleAlert,
-  AlertEvent,
-  TransactionEvent,
-  FindingSeverity,
-  FindingType,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType, getEthersProvider } from 'forta-agent';
+import { ethers } from 'ethers';
+import { Provider } from '@ethersproject/providers';
+import { LRUCache } from 'lru-cache';
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
+// Uniswap constants and ABI
+export const swapEvent =
+  'event Swap (address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)';
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
+export const UNISWAP_FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
+
+export const UNI_INIT_CODE = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54';
+
+export const uniswapPoolABI = [
+  'function token0() external view returns (address)',
+  'function token1() external view returns (address)',
+  'function fee() external view returns (uint24)',
+];
+
+// LRU Cache for pool address verification
+const poolAddressCache: LRUCache<string, boolean> = new LRUCache({ max: 1000000 });
+
+// Utility functions
+const getPoolValues = async (poolAddress: string, provider: Provider, blockNumber: number) => {
+  const poolContract = new ethers.Contract(poolAddress, uniswapPoolABI, provider);
+  const poolValues: any[] = await Promise.all([
+    poolContract.token0({ blockTag: blockNumber }),
+    poolContract.token1({ blockTag: blockNumber }),
+    poolContract.fee({ blockTag: blockNumber }),
+  ]);
+
+  return poolValues;
+};
+
+export const computePoolAddress = (factoryAddress: string, poolValues: any[]) => {
+  const encodedData = ethers.utils.defaultAbiCoder.encode(['address', 'address', 'uint24'], poolValues);
+  const salt = ethers.utils.solidityKeccak256(['bytes'], [encodedData]);
+  const poolAddress = ethers.utils.getCreate2Address(factoryAddress, salt, UNI_INIT_CODE);
+  return poolAddress;
+};
+
+const isUniswapPoolAddress = async (
+  factoryAddress: string,
+  poolAddress: string,
+  provider: Provider,
+  blockNumber: number
 ) => {
-  const findings: Finding[] = [];
+  if (poolAddressCache.get(poolAddress)) return poolAddressCache.get(poolAddress);
+  const poolValues = await getPoolValues(poolAddress, provider, blockNumber);
+  const computedPoolAddress = computePoolAddress(factoryAddress, poolValues);
+  const isUniswap = computedPoolAddress.toLowerCase() === poolAddress.toLowerCase();
+  poolAddressCache.set(poolAddress, isUniswap);
+  return isUniswap;
+};
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+// Forta bot logic
+export function provideHandleTransaction(
+  factoryAddress: string,
+  swapEvent: string,
+  provider: Provider
+): HandleTransaction {
+  return async function handleTransaction(txEvent: TransactionEvent) {
+    const findings: Finding[] = [];
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
+    const logs = txEvent.filterLog(swapEvent);
 
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
+    for (const log of logs) {
+      const poolAddress = log.address;
+      const isUniswapPool = await isUniswapPoolAddress(factoryAddress, poolAddress, provider, txEvent.blockNumber);
+      if (!isUniswapPool) {
+        continue;
+      }
 
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
+      const { sender, recipient, amount0, amount1, liquidity } = log.args;
+
       findings.push(
         Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
+          name: 'Uniswap V3 Swap Detector',
+          description: 'This Bot detects the Swaps executed on Uniswap V3',
+          alertId: 'FORTA-1',
+          severity: FindingSeverity.Info,
           type: FindingType.Info,
+          protocol: 'UniswapV3',
           metadata: {
-            to,
-            from,
+            poolAddress: poolAddress.toLowerCase(),
+            sender,
+            recipient,
+            amount0: amount0.toString(),
+            amount1: amount1.toString(),
+            liquidity: liquidity.toString(),
           },
         })
       );
-      findingsCount++;
     }
-  });
 
-  return findings;
-};
-
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
-
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
-
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
-
-// const healthCheck: HealthCheck = async () => {
-//   const errors: string[] = [];
-  // detect some health check condition
-  // errors.push("not healthy due to some condition")
-  // return errors;
-// }
+    return findings;
+  };
+}
 
 export default {
-  // initialize,
-  handleTransaction,
-  // healthCheck,
-  // handleBlock,
-  // handleAlert
+  provideHandleTransaction,
+  handleTransaction: provideHandleTransaction(UNISWAP_FACTORY_ADDRESS, swapEvent, getEthersProvider()),
 };
